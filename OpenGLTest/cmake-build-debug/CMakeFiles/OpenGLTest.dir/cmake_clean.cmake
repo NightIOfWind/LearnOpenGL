25c830c921254ec9fae473f88b16a2d5d6ba7e23
郑江涛 <zhengjiangtao@oasgames.com>
2021-06-09 19:37:30 +0800

    创建项目

---
+++ OpenGLTest/cmake-build-debug/CMakeFiles/OpenGLTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTest.dir/glad.c.o"
+  "CMakeFiles/OpenGLTest.dir/main.cpp.o"
+  "OpenGLTest"
+  "OpenGLTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OpenGLTest/cmake-build-debug/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/glad.c.o"
+  "CMakeFiles/demo.dir/main.cpp.o"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
